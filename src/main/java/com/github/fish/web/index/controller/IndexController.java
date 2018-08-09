d7f13b7b148c1eaf05b394cc2aae357a72669c8f
毒匠 <blackdancers@163.com>
2018-08-10 00:03:18 +0800

    about页面和页脚最新文章列表完善！

--- src/main/java/com/github/fish/web/index/controller/IndexController.java
+++ src/main/java/com/github/fish/web/index/controller/IndexController.java
@@ -37,24 +37,25 @@ public class IndexController {
 
         //文章列表
         PageInfo<Article> page = articleService.getArticleListByPage(pageable);
-        model.addAttribute("page",page);
+        model.addAttribute("page", page);
 
         List<Classification> classList = classificationService.getClassificationList(IConstInfo.DEFAULT_NUM);
-        model.addAttribute("classList",classList); //所属分类
+        model.addAttribute("classList", classList); //所属分类
 
         List<Tags> tagsList = tagsService.getTagsList(IConstInfo.DEFAULT_NUM);
-        model.addAttribute("tagsList",tagsList); //所有标签
+        model.addAttribute("tagsList", tagsList); //所有标签
 
         //推荐博客
         Article article = new Article();
         article.setIsRecommend(IConstInfo.ONE);
-        List<Article> recommendBlogList = articleService.getArticleList(article,IConstInfo.DEFAULT_NUM);
-        model.addAttribute("recommendBlogList",recommendBlogList);
+        List<Article> recommendBlogList = articleService.getArticleList(article, IConstInfo.DEFAULT_NUM);
+        model.addAttribute("recommendBlogList", recommendBlogList);
         return "index";
     }
 
     /**
      * 查询文章标题或内容
+     *
      * @param pageable
      * @param keyWords
      * @param model
@@ -62,15 +63,15 @@ public class IndexController {
      */
     @PostMapping("/search")
     public String searchBolgs(@PageableDefault(size = 10, sort = {"updateDate"}, direction = Sort.Direction.DESC) Pageable pageable,
-                             @RequestParam String keyWords, Model model) {
+                              @RequestParam String keyWords, Model model) {
 
         //设置搜索内容
         Article article = new Article();
         article.setKeyWords(keyWords);
         //文章列表
-        PageInfo<Article> page = articleService.getArticlePageBySearch(article,pageable);
-        model.addAttribute("page",page);
-        model.addAttribute("keyWords",keyWords); //查询参数
+        PageInfo<Article> page = articleService.getArticlePageBySearch(article, pageable);
+        model.addAttribute("page", page);
+        model.addAttribute("keyWords", keyWords); //查询参数
         return "search";
     }
 
@@ -79,17 +80,30 @@ public class IndexController {
 
         //文章列表
         Article article = articleService.getArticleContentById(articleId);
-        model.addAttribute("article",article);
+        model.addAttribute("article", article);
 
         List<Tags> tagsList = tagsService.getTagsListByArticleId(articleId);
-        model.addAttribute("tagsList",tagsList); //所有标签
+        model.addAttribute("tagsList", tagsList); //所有标签
         return "blog";
     }
 
+    @GetMapping("/about")
+    public String aboutMe(Model model) {
+        return "about";
+    }
 
 
-
-
+    /**
+     * 最新文章列表
+     */
+    @GetMapping("/footer/lastArticleList")
+    public String lastArticleList(Model model) {
+        Article article = new Article();
+        article.setIsRecommend(IConstInfo.ONE);
+        List<Article> lastArticleList = articleService.getArticleList(article, 3);
+        model.addAttribute("lastArticleList", lastArticleList);
+        return "_fragments :: lastArticleList";
+    }
 
 
 }
